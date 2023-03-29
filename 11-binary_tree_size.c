b795e84e8492ea5dba320f227124eadf979e71d8
swoji <davidookuku@gmail.com>
2023-03-29 16:05:58 +0300

    correction manadtory task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,16 +8,8 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size_l = 0, size_r = 0;
-
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		size_l = 1 + binary_tree_size(tree->left);
-
-	if (tree->right)
-		size_r = 1 + binary_tree_size(tree->right);
-
-	return (size_l + size_r);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
